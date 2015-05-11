168a10113258e8a6ecbc015315c4cd9aa88bb8ef
Kristján Pétursson <kristjan@gmail.com>
2015-05-10 23:10:32 -0700

    User profile banner
    Basic name and stats

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   def show
-    @user = RubyStackoverflow.users_with_answers([params[:id]]).data.first
+    @user = RubyStackoverflow.users_by_ids([params[:id]]).data.first
     render file: '/public/404', status: :not_found unless @user
   end
 end
