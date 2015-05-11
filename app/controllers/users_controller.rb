735e84728d0134f83688c8ba913f882432aa1c95
Kristján Pétursson <kristjan@gmail.com>
2015-05-11 00:09:05 -0700

    Add question and answer statistics
    Boy is it time to start testing. Here's
    - # questions asked
    - # answers given
    - Current streak of answering at least 1 question/day
    - Longest streak of the same

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,15 @@
 class UsersController < ApplicationController
   def show
     @user = RubyStackoverflow.users_by_ids([params[:id]]).data.first
-    render file: '/public/404', status: :not_found unless @user
+    render file: '/public/404', status: :not_found and return unless @user
+    questions = RubyStackoverflow.users_questions([@user.user_id]).data.first
+    @questions = questions.try(:questions) || []
+    answers = RubyStackoverflow.users_with_answers([@user.user_id]).data.first
+    @answers = answers.try(:answers) || []
+    answer_ids = @user.answers.map(&:id)
+    @answered = {}
+    if answer_ids.any?
+      @answered = RubyStackoverflow.questions_by_ids(answer_ids).index_by(&:id)
+    end
   end
 end
---
+++ app/helpers/users_helper.rb
@@ -0,0 +1,30 @@
+module UsersHelper
+  TIME_ZONE = 'Pacific Time (US & Canada)'
+  def answer_dates(answers)
+    answers.map do |answer|
+      answer.creation_date.to_time.in_time_zone(TIME_ZONE).to_date
+    end.sort.uniq.reverse
+  end
+
+  def current_streak(answers)
+    dates = answer_dates(answers)
+    return 0 if dates.empty?
+    return 0 unless dates.first.in?([today, today - 1])
+    required = dates.first
+    count = 0
+    count += 1 and required -= 1 while dates.shift == required
+    count
+  end
+
+  def date_slices(dates)
+    dates.slice_when { |newer, older| newer != older + 1 }
+  end
+
+  def longest_streak(answers)
+    date_slices(answer_dates(answers)).map(&:size).max
+  end
+
+  def today
+    Time.now.in_time_zone(TIME_ZONE).to_date
+  end
+end
