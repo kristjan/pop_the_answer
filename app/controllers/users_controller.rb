04f3e100a4c12632c54eb713548955243945924f
Kristján Pétursson <kristjan@gmail.com>
2015-05-16 08:45:42 -0700

    Attach questions and answers to user
    Realized I can concat these in and have fewer instance variables running
    around.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,13 +3,14 @@ class UsersController < ApplicationController
     @user = RubyStackoverflow.users_by_ids([params[:id]]).data.first
     render file: '/public/404', status: :not_found and return unless @user
     questions = RubyStackoverflow.users_questions([@user.user_id]).data.first
-    @questions = questions.try(:questions) || []
+    @user.questions.concat(questions.try(:questions) || [])
     answers = RubyStackoverflow.users_with_answers([@user.user_id]).data.first
-    @answers = answers.try(:answers) || []
-    answer_ids = @user.answers.map(&:id)
+    @user.answers.concat(answers.try(:answers) || [])
+    answer_ids = @user.answers.map(&:question_id)
     @answered = {}
     if answer_ids.any?
-      @answered = RubyStackoverflow.questions_by_ids(answer_ids).index_by(&:id)
+      @answered = RubyStackoverflow.questions_by_ids(answer_ids).data
+        .index_by(&:question_id)
     end
   end
 end
