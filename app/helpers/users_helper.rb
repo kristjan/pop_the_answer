830f0eb0d270cae4e9807f9900de35896086d75e
Kristján Pétursson <kristjan@gmail.com>
2015-05-16 13:44:22 -0700

    Organize answer history by month and show gaps

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -6,10 +6,10 @@ module UsersHelper
     end.sort.uniq.reverse
   end
 
-  def answers_by_date(answers)
+  def history(answers)
     answers.group_by do |answer|
       answer.creation_date.to_time.in_time_zone(TIME_ZONE).to_date
-    end.sort_by(&:first).reverse
+    end.sort_by(&:first).reverse.push(nil)
   end
 
   def current_streak(answers)
@@ -20,6 +20,24 @@ module UsersHelper
     count = 0
     count += 1 and required -= 1 while dates.shift == required
     count
+  end
+
+  # https://stackoverflow.com/questions/1065320/in-rails-display-time-between-two-dates-in-english
+  def date_difference(from_time, to_time)
+    from_time = from_time.to_time if from_time.respond_to?(:to_time)
+    to_time = to_time.to_time if to_time.respond_to?(:to_time)
+    distance_in_seconds = ((to_time - from_time).abs).round
+    components = []
+
+    %w(year month week day).each do |interval|
+      if distance_in_seconds >= 1.send(interval)
+        delta = (distance_in_seconds / 1.send(interval)).floor
+        distance_in_seconds -= delta.send(interval)
+        components << pluralize(delta, interval)
+      end
+    end
+
+    components.join(", ")
   end
 
   def date_slices(dates)
