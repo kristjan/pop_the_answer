e817f292231b555adc04112c1fa13574b4661698
Kristján Pétursson <kristjan@gmail.com>
2015-06-02 23:34:57 -0700

    Add paging to questions and answers
    Boy oh boy, it's really time to start pulling this out of the
    controller.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,16 +1,28 @@
 class UsersController < ApplicationController
+  PAGE_SIZE = 100
+
   def show
     @user = RubyStackoverflow.users_by_ids([params[:id]]).data.first
     render file: '/public/404', status: :not_found and return unless @user
     questions = RubyStackoverflow.users_questions([@user.user_id]).data.first
     @user.questions.concat(questions.try(:questions) || [])
-    answers = RubyStackoverflow.users_with_answers([@user.user_id]).data.first
-    @user.answers.concat(answers.try(:answers) || [])
+    page = 0
+    begin
+      page += 1
+      answers = RubyStackoverflow.users_with_answers(
+        [@user.user_id],
+        page: page,
+        pagesize: PAGE_SIZE
+      )
+      @user.answers.concat(answers.data.first.try(:answers) || [])
+    end while answers.has_more
     answer_ids = @user.answers.map(&:question_id)
     @answered = {}
-    if answer_ids.any?
-      @answered = RubyStackoverflow.questions_by_ids(answer_ids).data
-        .index_by(&:question_id)
+    answer_ids.in_groups_of(PAGE_SIZE, false).each do |ids|
+      answered_questions = RubyStackoverflow.questions_by_ids(
+        ids, pagesize: PAGE_SIZE
+      ).data
+      @answered.merge!(answered_questions.index_by(&:question_id))
     end
   end
 end
